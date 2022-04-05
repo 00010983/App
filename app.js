2d5fc2377faeb1770eca922440afb1f736e62e95
00010983 <95031998+00010983@users.noreply.github.com>
2022-04-05 22:01:52 +0500

    View engine exprirement

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT = 8000
 const fs = require('fs')
 
 //Viewing engine is pug with some addiotional ejs (HTML markup)
-app.set('view engine', 'pug')
+app.set('view engine', 'ejs')
 app.use('/static', express.static('public'))
 app.use(express.urlencoded( { 
     extended: false
